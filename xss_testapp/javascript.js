d81e7f21745686326821f82b5b8a0b72e4d33a02
jdh5202 <64127451+jdh5202@users.noreply.github.com>
2021-02-26 21:20:53 +0900

    Update javascript.js

--- xss_testapp/javascript.js
+++ xss_testapp/javascript.js
@@ -3,19 +3,25 @@
 var input_option = "input_opt1";
 var output_define = "";
 
-function ckENT(event){ 
-  var x = event.keyCode; 
-  if (x==13) { $("#confirm-button").trigger("click"); }
+function ckENT(event){ 
+  var x = event.keyCode; 
+  if (x==13) { $("#confirm-button").trigger("click"); }
 }
 
- 
+
+
 $(document).ready(function() {
 
+  
 $("#input_area").hide();
 $("#output_define").hide();
 $("#filter_define").hide();
 $("#code").hide();
+  
+$('input:radio[name="output_opt"]:checked').trigger("click");
 $("#preview_html").text("<h1>${INPUT_VALUE}</h1>");
+$('input:radio[name="output_opt"]:radio[id="output_opt1"]').prop('checked',true); $('input:checkbox[name="output_opt"]:checkbox[id="Filter_opt"]').prop('checked',false);
+$('input:radio[name="filter_system"]:radio[id="filter_system1"]').prop('checked',true); 
 
 
 $("#output_define").on('change keyup paste', function() 
@@ -31,16 +37,18 @@ $('input:radio[name="input_opt"]').on('click', function(e) {
 	{
 		$("#input").show();
 		$("#input_area").hide();
+		$(".hide_br").show();
 
 	} else {
 		$("#input_area").show();
 		$("#input").hide();
+    $(".hide_br").hide();
 	} input_option = $(this).attr('id');
 		
 });
 
 $('input:radio[name="output_opt"]').on('click', function(e) {
-
+  
 	var display_text = "";
 	var id = $(this).attr('id');
 	$("#output_define").hide();
@@ -73,7 +81,17 @@ $('input:radio[name="output_opt"]').on('click', function(e) {
 
 });
 
+$("input:radio[name='filter_system']").on('click', function(e) {
 
+  // blacklist check form message
+  if ( $(this)[0].id == 'filter_system2' )
+    { 
+      $("#filter_define")[0].placeholder="Whitelist filter Separator(,) => p,h1,a,script,font,br;";
+    } else { 
+      $("#filter_define")[0].placeholder="Blacklist filter Separator(,) => <,script,',%,eval";
+    }
+});
+  
 $('input:checkbox[id="Filter_opt3"]').on('click', function(e) {
 
 	if ( $(this).is(":checked") == false ) 
@@ -82,7 +100,7 @@ $('input:checkbox[id="Filter_opt3"]').on('click', function(e) {
 	} else { 
 	$("#filter_define").show();
 	}
-
+  
 });
 
 
@@ -117,17 +135,98 @@ function xss_filter(xss_val)
 
    return xss_val;
 }
+
+  
+function whitelist(xss_val)
+{
+  var filter_items = $("#filter_define").val();
+  
+  // filter
+    xss_val = xss_val.replaceAll("&", "&amp;")
+           .replaceAll("#", "&#35;")
+           .replaceAll(";", "&#59;")
+           .replaceAll("\\\\", "&#92;")
+           .replaceAll("<"  , "&lt;")
+           .replaceAll(">"  , "&gt;")
+           .replaceAll("\\(", "&#40;")
+           .replaceAll("\\)", "&#41;")
+           .replaceAll("[$]", "&#36;")
+           .replaceAll("[*]", "&#42;")
+           .replaceAll("[+]", "&#43;")
+           .replaceAll("[|]", "&#124;")
+           .replaceAll("\\." , "&#46;")
+           .replaceAll("\\?" , "&#63;")
+           .replaceAll("\\[" , "&#91;")
+           .replaceAll("\\]" , "&#93;")
+           .replaceAll("\\^" , "&#94;")
+           .replaceAll("\\{" , "&#123;")
+           .replaceAll("\\}" , "&#125;")
+           .replaceAll("!", "&#33;")
+           .replaceAll("%", "&#37;")
+           .replaceAll(",", "&#44;")
+           .replaceAll("-", "&#45;")
+           .replaceAll(":", "&#58;")
+           .replaceAll("@", "&#64;")
+           .replaceAll("_", "&#95;")
+           .replaceAll("`", "&#96;")
+           .replaceAll("~", "&#126;");
+
+  if ( filter_items )
+  {
+    
+
+    // white list - get input value
+     var filter_items = filter_items.split(',');
+     var tag = [];
+
+     for ( var i = 0; i < filter_items.length; i++ ) { 
+
+    if ( filter_items[i].indexOf(':') == -1 )
+      tag.push( filter_items[i] );
+     } 
+
+       var exp_var = '(' + tag.join('|') + ')';
+
+     // apply tag
+     var expre = `&lt;\\s*\/?\\s*${exp_var}\\b.*?&gt;`;
+
+     var reg = new RegExp(expre,"gi");
+
+     var real_data = '';
+     var filter_data = '';
+
+     $.each ( xss_val.match(reg) , function(i,res_data) {
+
+     real_data = res_data.replaceAll('&lt;','<')
+               .replaceAll('&gt;','>');
+     xss_val = xss_val.replaceAll( res_data , real_data );
+
+     }); 
+  
+  } return xss_val;
+}
+  
 // 1 - OK 
 function codeOutput(xss_val) {
 
-  xss_val = xss_filter(xss_val);
-   
+  if ( $('input:radio[id="filter_system1"]').is(":checked") ) 
+  { xss_val = xss_filter(xss_val); } 
+  else { xss_val = whitelist( xss_val ); }
   if ( xss_val = output_option(xss_val) )
-  {
+  { 
 	  $("#code").show();    
 	  $("#code-title").html("<p><b><font size='4.2em'>[Output code]</font></b></p>");
 	  $("#code").text(xss_val);
 	  $("#output-html").html(xss_val);
+    $("#output-html").append( `<script> $(document).ready(function() { 
+ 
+     $('#output-html a').on('click', function(e) {
+	
+       // event.preventDefault();
+
+	}); });<\/script>` );
+    
+    
   }
 }
 
@@ -156,8 +255,6 @@ function output_option(xss_val) {
   
 }
 
-
-
 $('#confirm-button').on('click', function(e) {
   e.preventDefault();
   
@@ -167,7 +264,10 @@ $('#confirm-button').on('click', function(e) {
   if ( input_option == "input_opt1" )
   {
     xss_val = $("#input").val();
-  } else { xss_val = $("#input_area").val(); }
+  } else { 
+    xss_val = $("#input_area").val(); 
+    xss_val = xss_val.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+  }
   
   codeOutput(xss_val);
     
@@ -175,5 +275,7 @@ $('#confirm-button').on('click', function(e) {
  });
 
 });
+
+
 
 </script>
