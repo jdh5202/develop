1257b0e3746ce4551f920ff39aa87b705cd6ea22
jdh5202 <64127451+jdh5202@users.noreply.github.com>
2021-07-25 17:37:46 +0900

    Update append_image.ps1

--- 이미지 작업/append_image.ps1
+++ 이미지 작업/append_image.ps1
@@ -10,7 +10,7 @@ Param ([String]$big_size_image, [String]$result_img_x, [String]$result_img_y, [S
     # 사이즈가 큰 이미지 파일을 축소하여 임시 이미지 파일로 저장
     $result_img = $result_img_x + "x" + $result_img_y
     magick convert $big_size_image -resize $result_img -quality 100 $cache_file_name
-    
+     
     # 임시 이미지 파일 사이즈 획득
     $cache_file_x = magick identify -format '%w' $cache_file_name
     $cache_file_y = magick identify -format '%h' $cache_file_name
@@ -18,6 +18,7 @@ Param ([String]$big_size_image, [String]$result_img_x, [String]$result_img_y, [S
     $cache_img_size = ""
     if ( $img_dir -eq "w" ) { $cache_img_size = [String](([Int]$cache_file_x)+$white_space) + "x" + $cache_file_y }
     elseif ( $img_dir -eq "h" ) { $cache_img_size = $cache_file_x + "x" + [String](([int]$cache_file_y+$white_space)) }
+    echo $cache_img_size
 
     # 임시 이미지 파일에 여백 추가
     ## 가로
@@ -25,8 +26,8 @@ Param ([String]$big_size_image, [String]$result_img_x, [String]$result_img_y, [S
     elseif ( $direction -eq "right" -and $img_dir -eq "w" ) { magick convert -size $cache_img_size xc:white $cache_file_name -gravity east -composite $cache_file_name } 
     
     ## 세로
-    if ( $direction -eq "left" -and $img_dir -eq "h" ) { magick convert -size $cache_img_size xc:white $cache_file_name -gravity north -composite $cache_file_name }
-    elseif ( $direction -eq "right" -and $img_dir -eq "h" ) { magick convert -size $cache_img_size xc:white $cache_file_name -gravity south -composite $cache_file_name } 
+    if ( $direction -eq "left" -and $img_dir -eq "h" ) { magick convert -size $cache_img_size xc:white $cache_file_name -gravity south -composite $cache_file_name }
+    elseif ( $direction -eq "right" -and $img_dir -eq "h" ) { magick convert -size $cache_img_size xc:white $cache_file_name -gravity north -composite $cache_file_name }
 
     # 이미지 이어붙이기
     ## 가로
@@ -34,11 +35,11 @@ Param ([String]$big_size_image, [String]$result_img_x, [String]$result_img_y, [S
     elseif ( $direction -eq "right" -and $img_dir -eq "w" ) { magick convert +append $img_file $cache_file_name $result_img_name }
     
     ## 세로
-    if ( $direction -eq "left" -and $img_dir -eq "h" ) { magick convert -append $cache_file_name $img_file $result_img_name }
-    elseif ( $direction -eq "right" -and $img_dir -eq "h" ) { magick convert -append $img_file $cache_file_name $result_img_name }
+    if ( $direction -eq "left" -and $img_dir -eq "h" ) { magick convert -append $img_file $cache_file_name $result_img_name }
+    elseif ( $direction -eq "right" -and $img_dir -eq "h" ) { magick convert -append $cache_file_name $img_file $result_img_name }
 
     # 임시 이미지 파일 삭제
-    Remove-Item $cache_file_name
+    #Remove-Item $cache_file_name
 
     # 출력
     echo "Create $result_img_name"
@@ -62,12 +63,11 @@ $right_img_y = magick identify -format '%h' $file_name2
     {
            # 오른쪽 이미지 가로 사이즈가 더 클 경우
            if ( [int]$left_img_x -lt [int]$right_img_x ) 
-           {
-              marge -big_size_image $file_name1 -result_img_x $right_img_x -result_img_y $right_img_y -img_file $file_name2 -white_space $white_space -direction "left" -img_dir "h"
-           } else { 
-              marge -big_size_image $file_name2 -result_img_x $left_img_x -result_img_y $left_img_x -img_file $file_name1 -white_space $white_space -direction "right" -img_dir "h"
+           {  
+              marge -big_size_image $file_name2 -result_img_x $left_img_x -result_img_y $left_img_y -img_file $file_name1 -white_space $white_space -direction "left" -img_dir "h"
+           } else { echo "hi"
+              marge -big_size_image $file_name1 -result_img_x $right_img_x -result_img_y $right_img_y -img_file $file_name2 -white_space $white_space -direction "right" -img_dir "h"
            }
-
 
     } else {
        
