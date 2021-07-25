c60fccca789ab228e700289f2beed6496dddddbd
jdh5202 <64127451+jdh5202@users.noreply.github.com>
2021-07-25 19:02:44 +0900

    Create zoomin_out.ps1

---
+++ 이미지 작업/zoomin_out.ps1
@@ -0,0 +1,23 @@
+param([string]$file_name, [String]$resize_x, $resize_y=100 )
+
+if ( $file_name -ne "" -and $resize_x -ne "" )
+{
+
+   $file_name = $file_name -replace '.\\',''
+   $resize_size = ""
+   if ( $resize_x -match "x" )
+   {
+        $resize_size = [String]$resize_x + 'x' + [String]$resize_y + '!'
+   } else { 
+        $resize_size = [String]$resize_x + '%x' + [String]$resize_y + '%'
+   }
+        $result_img_name = ($file_name -replace [System.IO.Path]::GetExtension($file_name)) + "_zoom" + [System.IO.Path]::GetExtension($file_name)    
+        magick convert $file_name -resize $resize_size -quality 100 $result_img_name     
+
+} else {  
+echo ""
+echo "파일 이름 및 확대/축소 하려는 이미지의 사이즈를 입력하세요." 
+echo 'example(2배축소)> zoomin_out.ps1 [image.png] [50 - x], [50 - y]'
+echo 'example(크기지정)> zoomin_out.ps1 [image.png] [250x350 - (x)x(y)]'
+echo ""
+}
