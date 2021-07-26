37b7389ec32e4aaf4df769942a9e1664b198f4a1
jdh5202 <64127451+jdh5202@users.noreply.github.com>
2021-07-26 23:20:30 +0900

    Update append_image.ps1

--- 이미지 작업/append_image.ps1
+++ 이미지 작업/append_image.ps1
@@ -6,15 +6,18 @@ Param ([String]$big_size_image, [String]$result_img_x, [String]$result_img_y, [S
      # 임시 이미지 파일 이름, 최종 파일명
     $cache_file_name = "tmp_cache_img.png"
     $result_img_name = ($big_size_image -replace [System.IO.Path]::GetExtension($big_size_image)) + "_apd" + [System.IO.Path]::GetExtension($big_size_image)
-
+    $result_img = ""
     # 사이즈가 큰 이미지 파일을 축소하여 임시 이미지 파일로 저장
-    $result_img = $result_img_x + "x" + $result_img_y
+    
+    if ( $img_dir -eq "w" ) { $result_img = $result_img_x + "x" + $result_img_y }
+    elseif ( $img_dir -eq "h" ) { $result_img = $result_img_x }
+    
     magick convert $big_size_image -resize $result_img -quality 100 $cache_file_name
-     
+   
     # 임시 이미지 파일 사이즈 획득
     $cache_file_x = magick identify -format '%w' $cache_file_name
     $cache_file_y = magick identify -format '%h' $cache_file_name
-    
+
     $cache_img_size = ""
     if ( $img_dir -eq "w" ) { $cache_img_size = [String](([Int]$cache_file_x)+$white_space) + "x" + $cache_file_y }
     elseif ( $img_dir -eq "h" ) { $cache_img_size = $cache_file_x + "x" + [String](([int]$cache_file_y+$white_space)) }
@@ -65,6 +68,7 @@ $right_img_y = magick identify -format '%h' $file_name2
            {  
               marge -big_size_image $file_name2 -result_img_x $left_img_x -result_img_y $left_img_y -img_file $file_name1 -white_space $white_space -direction "left" -img_dir "h"
            } else { 
+           echo "aaa"
               marge -big_size_image $file_name1 -result_img_x $right_img_x -result_img_y $right_img_y -img_file $file_name2 -white_space $white_space -direction "right" -img_dir "h"
            }
 
